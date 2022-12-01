bcc893703bd7d3c663dbcfffc9f021c470f7e3a2
Your Name <jg2262@cornell.edu>
2022-12-01 00:17:42 -0500

    inital commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Boomer!  -e WATCHPACK_POLLING=true is required to enable auto refresh!
+        </p>
+        <p> Really </p>
+        <p>
+          By here, docker compose should be ready. The run command became much easier
         </p>
         <a
           className="App-link"
